b9f15ac4060cbcf2ed1d1c4c43518a4788a5496f
azrael1998 <vipinachar2020@gmail.com>
2021-10-06 14:23:40 +0530

    v1

--- jsdjango/settings.py
+++ jsdjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@eppy+m!2_^u$w(@%63h24lfbp!(z_mfv1tq%wyq&-f5%9bx_n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [' https://37e7-43-247-157-2.ngrok.io ',
 '37e7-43-247-157-2.ngrok.io',
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = [' https://37e7-43-247-157-2.ngrok.io ',
 'localhost',
 'ff3d-43-247-157-2.ngrok.io',
 '081b-43-247-157-2.ngrok.io',
+'5301-43-247-157-2.ngrok.io',
+'5301-43-247-157-2.ngrok.io',
 ]
 
 
